8e99b6855c2174adb334beb5dba2bc15995bbcc4
Alex Eidt <alex.eidt@outlook.com>
2021-01-08 22:39:22 -0800

    Added more in-depth project descriptions

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Toggle Main Image when clicked
+// Toggle Main Image when clicked.
 function toggle(self) {
     const h1 = document.getElementById('mainHeader');
     const mainImage = document.getElementsByClassName('mainFrame')[0];
@@ -6,23 +6,47 @@ function toggle(self) {
 
     switch (src) {
         case 'mainIcon.png':
-            mainImage.style.width = "75%";
-            h1.style['padding-top'] = "10vh";
+            mainImage.style.width = '75%';
+            h1.style['padding-top'] = '10vh';
             mainImage.src = 'Icons/alexeidt.gif';
+            h1.style.visibility = 'hidden';
+            break;
         case 'python.png':
-            if (src === 'python.png') {
-                mainImage.style.width = "100%";
-                h1.style['padding-top'] = "0vh";
-                mainImage.src = 'Icons/pythons.gif';
-            }
+            mainImage.style.width = '100%';
+            h1.style['padding-top'] = '0vh';
+            mainImage.src = 'Icons/pythons.gif';
             h1.style.visibility = 'hidden';
             break;
         case 'alexeidt.gif':
         case 'pythons.gif':
             mainImage.style.width = "85%";
             h1.style.visibility = 'visible';
-            h1.style['padding-top'] = "30vh";
+            h1.style['padding-top'] = '30vh';
             mainImage.src = 'Icons/mainIcon.png';
             break;
     }
+}
+
+// Toggle Left side of screen when Link is clicked.
+function toggleLeft(self) {
+    const left = document.getElementsByClassName('left')[0];
+    if (left.style.width === '0%') {
+        return null;
+    }
+    // Scroll to top of left container on click.
+    left.scroll({
+        top: 0, 
+        left: 0, 
+        behavior: 'smooth'
+    });
+    if (left.innerHTML.includes('Alex Eidt')) {
+        document.getElementById('storage').innerHTML = left.innerHTML;
+    }
+    const leftHeader = left.getElementsByClassName('left-header');
+    if (leftHeader.length > 0 && leftHeader[0].innerHTML.includes(self.innerHTML)) {
+        left.innerHTML = document.getElementById('storage').innerHTML;
+    } else {
+        left.innerHTML = document.getElementById(self.innerHTML).innerHTML;
+    }
+    return null;
 }
