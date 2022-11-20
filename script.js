542830557f8f57da6bd23c4578b4362226046f92
Alex Eidt <alex.eidt@outlook.com>
2022-11-19 19:38:17 -0800

    Cleanup

--- script.js
+++ script.js
@@ -19,42 +19,42 @@ for (let i = 0; i < 8; i++) {
 
 function drawCube(e) {
     const board = document.getElementById('canvas');
-    let width = board.width;
-    let height = board.height;
+    const width = board.width;
+    const height = board.height;
 
     const ctx = board.getContext('2d');
     ctx.clearRect(0, 0, width, height);
 
-    let angleX = e.pageX / window.innerWidth * 4;
-    let angleY = e.pageY / window.innerHeight * 4;
+    const angleX = -e.pageX / window.innerWidth * 4;
+    const angleY = -e.pageY / window.innerHeight * 4;
 
-    let cosX = Math.cos(angleX);
-    let sinX = Math.sin(angleX);
-    let cosY = Math.cos(angleY);
-    let sinY = Math.sin(angleY);
-    let cosZ = Math.cos(angleX * angleY);
-    let sinZ = Math.sin(angleX * angleY);
+    const cosX = Math.cos(angleX);
+    const sinX = Math.sin(angleX);
+    const cosY = Math.cos(angleY);
+    const sinY = Math.sin(angleY);
+    const cosZ = Math.cos(angleX + angleY);
+    const sinZ = Math.sin(angleX + angleY);
 
-    let m00 = cosY*cosZ;
-    let m01 = -cosY*sinZ;
-    let m02 = sinY;
-    let m10 = cosX*sinZ + sinX*sinY*cosZ;
-    let m11 = cosX*cosZ - sinX*sinY*sinZ;
-    let m12 = -sinX*cosY;
-    let m20 = sinX*sinZ - cosX*sinY*cosZ;
-    let m21 = sinX*cosZ + cosX*sinY*sinZ;
-    let m22 = cosX*cosY;
+    const m00 = cosY*cosZ;
+    const m01 = -cosY*sinZ;
+    const m02 = sinY;
+    const m10 = cosX*sinZ + sinX*sinY*cosZ;
+    const m11 = cosX*cosZ - sinX*sinY*sinZ;
+    const m12 = -sinX*cosY;
+    const m20 = sinX*sinZ - cosX*sinY*cosZ;
+    const m21 = sinX*cosZ + cosX*sinY*sinZ;
+    const m22 = cosX*cosY;
 
     let proj = [];
     for (let v of cube) {
-        let rx = m00*v[0] + m01*v[1] + m02*v[2];
-        let ry = m10*v[0] + m11*v[1] + m12*v[2];
-        let rz = m20*v[0] + m21*v[1] + m22*v[2];
+        const rx = m00*v[0] + m01*v[1] + m02*v[2];
+        const ry = m10*v[0] + m11*v[1] + m12*v[2];
+        const rz = m20*v[0] + m21*v[1] + m22*v[2];
 
-        let z = 1 / (2 - rz);
+        const z = 1 / (2 - rz);
 
-        let px = rx*z * (width / 2) + (width / 2);
-        let py = ry*z * (width / 2) + (height / 2);
+        const px = rx*z * (width / 2) + (width / 2);
+        const py = ry*z * (width / 2) + (height / 2);
 
         proj.push([px, py]);
     }
@@ -71,8 +71,6 @@ function drawCube(e) {
         drawLine(ctx, proj[i], proj[i + 2]);
         drawLine(ctx, proj[i + 4], proj[i + 6]);
     }
-
-    // window.requestAnimationFrame(drawCube);
 }
 
 function drawLine(ctx, a, b) {
